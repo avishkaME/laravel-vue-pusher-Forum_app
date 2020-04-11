e2dbfe5183ec246302463d3ecff62ea7ba39762d
avishka meddegoda <ameddegoda143@gmail.com>
2020-04-11 22:20:06 +0530

    Likes and Unlike system for Replies

--- resources/js/components/reply/Reply.vue
+++ resources/js/components/reply/Reply.vue
@@ -4,6 +4,8 @@
             <v-card-title>
                 <div class="headline">{{data.user}}</div>
                 <div class="ml-2">said {{data.created_at}}</div>
+                <v-spacer></v-spacer>
+                <like :content="data"></like>
             </v-card-title>
             <v-divider></v-divider>
 
@@ -26,6 +28,7 @@
 
 <script>
 import EditReply from './EditReply'
+import Like from '../likes/Like'
     export default {
         data() {
             return {
@@ -34,7 +37,7 @@ import EditReply from './EditReply'
             }
         },
         props: ['data','index'],
-        components: {EditReply},
+        components: {EditReply,Like},
         computed: {
             own() {
                 return User.own(this.data.user_id)
